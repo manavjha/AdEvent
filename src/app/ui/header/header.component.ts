33b5a559c281ccfc568df68206e659ce986f7f3e
Manu Jha <manu.jha@irissoftware.com>
2019-05-29 12:07:58 +0530

    commited

--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -8,8 +8,46 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
-
+menu=[{
+name:"Home",
+imgUrl:"",
+  count: 7,
+id:1
+},
+  {
+    name: "Upcoming Events",
+    imgUrl: "calender",
+    count:5,
+    id:2
+  }, 
+  {
+    name: "Past Events",
+    imgUrl: "",
+    count: 10,
+    id:3
+  },
+  {
+    name: "Destinations",
+    imgUrl: "",
+    count: 11,
+    id:4
+  },
+  {
+    name: "Experience",
+    imgUrl: "",
+    count: 4,
+    id:5
+  },
+  {
+    name: "Contact Us",
+    imgUrl: "",
+    count: 3,
+    id:6
+  },
+];
   ngOnInit() {
+
+    
   }
 
 }
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LayoutComponent, FooterComponent, HeaderComponent],
+  declarations: [LayoutComponent, FooterComponent, HeaderComponent, SliderComponent],
   exports: [LayoutComponent]
 })
 export class UiModule { }
